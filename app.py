56f3b484955bb7159cfd35944f9b71ea865b4f04
姚韋辰 <yaoweichen@yaoweichendeMBP.Home>
2017-10-12 00:23:24 +0800

    Init

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
