365d1c91a2c5780205c94ed94d1e74058ce4ace2
姚韋辰 <yaoweichen@yaoweichendeMBP.Home>
2017-10-12 00:45:35 +0800

    Init

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ def callback():
     return 'OK'
 
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text))
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 import os
 if __name__ == "__main__":
