49c8ffd73cfcb2854a6df52f8be32d8603755391
Wei-Chen Yao <yaoandy107@gmail.com>
2017-12-31 02:21:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         message)
