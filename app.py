b05b108a185adbdd7440218598335470d0a95281
yaoandy107 <yaoandy107@gmail.com>
2018-07-07 02:36:15 +0800

    Optimize code

--- app.py
+++ app.py
@@ -20,26 +20,21 @@ handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
